8866c4d931b2a5012f69cd82a6a491de8b3f608d
jonayed-a-saif-dev <jonayed.stud05@gmail.com>
2025-07-06 16:12:49 +0600

    Initial Files

--- src/components/CartSidebar.jsx
+++ src/components/CartSidebar.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import CartItem from './CartItem'; 
 import { clearCart } from '../store/cartSlice';
 
+//function
 function CartSidebar({isOpen, onClose}) {
 
   const dispatch = useDispatch();
